8aaff81fd859316b4afb15c7675c33b64e1b4c90
Tungtom2004 <114893594+Tungtom2004@users.noreply.github.com>
2024-11-29 20:51:46 +0700

    Update Classes.java

--- src/stu_mng_sys/Classes.java
+++ src/stu_mng_sys/Classes.java
@@ -11,11 +11,15 @@ public class Classes {
         this.students = student;
     }
 
-    public void addNewClass(){
-        
+    public void addNewClass(Student student){
+        students.add(student);
+        System.out.println("Student " + student.getFullName() + " has been added to class " + classID);
     }
     
     public void viewClassInfor(){
-        
+        System.out.println("Students in this class:");
+        for (Student student : students) {
+            System.out.println(" - " + student);
+        }
     }  
 }
